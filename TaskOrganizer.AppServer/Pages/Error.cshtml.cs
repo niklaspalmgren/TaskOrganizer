0d564aa47a8322c0de336f0707cc735919b669c5
NiklasPalmgren <niklas.palmgren@strusoft.com>
2022-03-14 18:03:03 +0100

    Refactoring.

--- TaskOrganizer.AppServer/Pages/Error.cshtml.cs
+++ TaskOrganizer.AppServer/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace webStep.AppServer.Pages
+namespace TaskOrganizer.AppServer.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
