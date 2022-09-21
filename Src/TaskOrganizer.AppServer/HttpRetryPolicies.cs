418d6a10a12f07df6b57fe9d74a4a72403bb619a
Niklas Palmgren <9886401+niklaspalmgren@users.noreply.github.com>
2022-09-21 09:39:29 +0200

    Added and implemented retry policies.

---
+++ Src/TaskOrganizer.AppServer/HttpRetryPolicies.cs
@@ -0,0 +1,16 @@
+﻿using Polly.Contrib.WaitAndRetry;
+using Polly.Retry;
+using Polly;
+using System.Net;
+
+namespace TaskOrganizer.AppServer
+{
+    internal static class HttpRetryPolicies
+    {
+        internal static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy => Policy<HttpResponseMessage>
+            .Handle<HttpRequestException>()
+            .OrResult(x => x.StatusCode is >= HttpStatusCode.InternalServerError or HttpStatusCode.RequestTimeout)
+            .WaitAndRetryAsync(Backoff.DecorrelatedJitterBackoffV2(TimeSpan.FromSeconds(1), 5));
+
+    }
+}
--- Src/TaskOrganizer.AppServer/ServiceExtensions.cs
+++ Src/TaskOrganizer.AppServer/ServiceExtensions.cs
@@ -1,29 +1,36 @@
-﻿namespace TaskOrganizer.AppServer;
+﻿using Polly;
+using Polly.Contrib.WaitAndRetry;
+using System.Net;
+
+namespace TaskOrganizer.AppServer;
 
 public static class ServiceExtensions
 {
+
+    /// <summary>
+    /// Adds http client services.
+    /// </summary>
     public static IServiceCollection AddHttpClients(this IServiceCollection services, ConfigurationManager configurationManager)
-    {
-        // Http client configurations
+    {        
         services.AddHttpClient("default", c =>
         {
             c.BaseAddress = new Uri(configurationManager.GetValue<string>("ApiBaseUri"));
-        });
+        }).AddPolicyHandler(HttpRetryPolicies.GetRetryPolicy);
 
         services.AddHttpClient("Tasks", c =>
         {
             c.BaseAddress = new Uri(configurationManager.GetValue<string>("TasksApiBaseUri"));
-        });
+        }).AddPolicyHandler(HttpRetryPolicies.GetRetryPolicy);
 
         services.AddHttpClient("Users", c =>
         {
             c.BaseAddress = new Uri(configurationManager.GetValue<string>("UsersApiBaseUri"));
-        });
+        }).AddPolicyHandler(HttpRetryPolicies.GetRetryPolicy);
 
         services.AddHttpClient("TaskBoards", c =>
         {
             c.BaseAddress = new Uri(configurationManager.GetValue<string>("TaskBoardsApiBaseUri"));
-        });
+        }).AddPolicyHandler(HttpRetryPolicies.GetRetryPolicy);
 
         return services;
     }
